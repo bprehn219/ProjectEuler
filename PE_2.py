ebe3fee9f85d23ba7b13108d9b063e2769aa93cf
bprehn219 <bprehn219@gmail.com>
2021-03-18 21:44:17 -0600

    Update PE_2.py

--- PE_2.py
+++ PE_2.py
@@ -2,23 +2,24 @@ def fibonacci_evens(max_term):
   last = 1
   current = 2
   running_sum = long()
-for i in 1:maxterm:
-  if current == max_term:
-    if current % 2 == 0:
-      running_sum = running_sum + current
-      return running_sum
+  while current <= max_term:
+    nxt = last + current
+    if current == max_term or nxt > max_term:
+      if current % 2 == 0:
+        running_sum = running_sum + current
+        return running_sum
+      else:
+        return running_sum
     else:
-      return running_sum
-  else:
-    if current % 2 == 0:
-      running_sum = running_sum + current
-      next = last + current
-      last = current
-      current = next
+      if current % 2 == 0:
+        running_sum = running_sum + current
+        last = current
+        current = nxt
       
-    else
-    current = last + current
-    last = current
-    if 
-    return
+      else:
+        nxt = last + current
+        last = current
+      
+    current = nxt
   
+print(fibonacci_evens(4000000))
